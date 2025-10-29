c5dd9f73b56f13167ae159c9eac47250faeb1b85
imostack <sampsonimoyin@gmail.com>
2025-10-29 12:15:11 +0100

    Fix Next.js build for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 };
 
 export default nextConfig;
