de297e3a0f961cd59bc50eb627be4b8a0aaa3992
jaysampson <alprosel@aol.com>
2025-11-11 17:50:44 +0100

    changes to pricing

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,6 +1,6 @@
 // app/pricing/page.tsx
 import { Header } from "@/components/header"
-import Footer from "@/components/ui/footer"
+import Footer from "@/components/footer"
 
 export default function PricingPage() {
   return (
