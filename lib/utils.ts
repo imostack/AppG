dafb3e62d238abc8340dcd306f9aa0c998a02a9c
jaysampson <alprosel@aol.com>
2025-11-11 17:03:44 +0100

    Made changes to utils.ts for future analytics use

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Returns true if the user has accepted cookies
+export function hasCookieConsent(): boolean {
+  if (typeof window === "undefined") return false
+  return localStorage.getItem("cookie-consent") === "accepted"
+}
