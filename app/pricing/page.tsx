c82c8712edb643f90fe284df3e9d2708bb5c0782
jaysampson <alprosel@aol.com>
2025-11-11 17:53:27 +0100

    Updated pricing

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,6 +1,6 @@
 // app/pricing/page.tsx
 import { Header } from "@/components/header"
-import Footer from "@/components/footer"
+import { Footer } from "@/components/footer"
 
 export default function PricingPage() {
   return (
